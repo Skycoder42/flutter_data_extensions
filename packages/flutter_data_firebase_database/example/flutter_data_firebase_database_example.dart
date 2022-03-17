01deebd1afb4147345a9d083e37c1d31fd7d86b8
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-03-17 17:24:39 +0100

    setup project

---
+++ packages/flutter_data_firebase_database/example/flutter_data_firebase_database_example.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_data_firebase_database/flutter_data_firebase_database.dart';
+
+void main() {
+  final awesome = Awesome();
+  // ignore: avoid_print
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/flutter_data_firebase_database/lib/flutter_data_firebase_database.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library flutter_data_firebase_database;
+
+export 'src/flutter_data_firebase_database_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
