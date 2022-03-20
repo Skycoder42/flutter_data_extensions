e658b13e371fe029afb4b81264445b3c0848391b
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-03-20 12:03:41 +0100

    use universal:io in test for web support

--- packages/flutter_data_firebase_database/test/integration/setup/di_setup.dart
+++ packages/flutter_data_firebase_database/test/integration/setup/di_setup.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter_data/flutter_data.dart';
 import 'package:meta/meta.dart';
+import 'package:universal_io/io.dart';
 
 import '../repositories/test.data.dart';
 import 'setup.dart';
