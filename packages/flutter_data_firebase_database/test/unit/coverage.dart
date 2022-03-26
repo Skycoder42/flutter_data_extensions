ebe85796063a965e2715e04158275acd78a6fa1e
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-03-26 17:30:59 +0100

    add coverage test helper

---
+++ packages/flutter_data_firebase_database/test/unit/coverage.dart
@@ -0,0 +1,9 @@
+// ignore: test_library_import
+import 'package:flutter_data_firebase_database/flutter_data_firebase_database.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('ensure coverage', () {
+    expect(FirebaseDatabaseAdapter, anything);
+  });
+}
