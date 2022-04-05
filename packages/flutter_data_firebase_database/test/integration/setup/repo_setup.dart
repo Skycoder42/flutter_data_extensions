48fc4a41a11bc96fe24a7fcbe7ed00012012c026
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-04-05 08:44:37 +0200

    .

--- packages/flutter_data_firebase_database/test/integration/setup/repo_setup.dart
+++ packages/flutter_data_firebase_database/test/integration/setup/repo_setup.dart
@@ -1,3 +1,7 @@
+// ignore: test_library_import
+import 'package:flutter_data_firebase_database/flutter_data_firebase_database.dart';
+import 'package:hive/hive.dart';
+
 import '../repositories/test.data.dart';
 import 'database_setup.dart';
 
@@ -5,6 +9,10 @@ mixin RepoSetup on DatabaseSetup {
   @override
   Future<void> setUpAll() async {
     await super.setUpAll();
+
+    Hive
+      ..registerAdapter(const ServerTimestampHiveAdapter())
+      ..registerAdapter(const ServerIncrementableHiveAdapter<double>());
 
     await di.read(repositoryInitializerProvider(verbose: true).future);
   }
--- packages/flutter_data_firebase_database/test/integration/setup/test_repository_setup.dart
+++ packages/flutter_data_firebase_database/test/integration/setup/test_repository_setup.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_data/flutter_data.dart';
 // ignore: test_library_import
-import 'package:flutter_data_firebase_database/flutter_data_firebase_database.dart';
-import 'package:hive/hive.dart';
 
 import '../repositories/test_repository.dart';
 import 'account_setup.dart';
@@ -18,10 +16,6 @@ class TestRepositorySetup extends Setup
   @override
   Future<void> setUpAll() async {
     await super.setUpAll();
-
-    Hive
-      ..registerAdapter(const ServerTimestampHiveAdapter())
-      ..registerAdapter(const ServerIncrementableHiveAdapter<double>());
 
     repository = di.read(testModelsRepositoryProvider);
   }
