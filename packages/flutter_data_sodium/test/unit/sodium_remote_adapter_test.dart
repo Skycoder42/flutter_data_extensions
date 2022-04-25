cb94abf3aa4b74630a9c585cfe5520ade71134aa
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-04-25 07:59:25 +0200

    add sodium remote adapter tests

---
+++ packages/flutter_data_sodium/test/unit/sodium_remote_adapter_test.dart
@@ -0,0 +1,186 @@
+import 'dart:typed_data';
+
+import 'package:flutter_data/flutter_data.dart';
+import 'package:flutter_data_sodium/src/encryption/data_cipher.dart';
+import 'package:flutter_data_sodium/src/encryption/encrypted_data.dart';
+import 'package:flutter_data_sodium/src/key_management/key_manager.dart';
+import 'package:flutter_data_sodium/src/sodium_remote_adapter.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:sodium/sodium.dart';
+import 'package:test/test.dart';
+
+class TestDataModel extends DataModel<TestDataModel> {
+  @override
+  final Object? id;
+
+  TestDataModel(this.id);
+}
+
+class MockSodium extends Mock implements Sodium {}
+
+class MockKeyManager extends Mock implements KeyManager {}
+
+class MockDataCipher extends Mock implements DataCipher {}
+
+class MockRemoteAdapter<T extends DataModel<T>> extends Mock
+    implements RemoteAdapter<T> {
+  Map<String, dynamic> mockSerialize(T model) => noSuchMethod(
+        Invocation.method(
+          const Symbol('serialize'),
+          [model],
+        ),
+      ) as Map<String, dynamic>;
+
+  DeserializedData<T> mockDeserialize(Object? data, {String? key}) =>
+      noSuchMethod(
+        Invocation.method(
+          const Symbol('deserialize'),
+          [data],
+          {const Symbol('key'): key},
+        ),
+      ) as DeserializedData<T>;
+}
+
+class SutRemoteAdapter extends MockRemoteAdapter<TestDataModel>
+    with SodiumRemoteAdapter<TestDataModel> {
+  final DataCipher? cipherOverride;
+
+  SutRemoteAdapter([this.cipherOverride]);
+
+  @override
+  DataCipher get cipher => cipherOverride ?? super.cipher;
+}
+
+class FakeEncryptedData extends Fake implements EncryptedData {
+  final Map<String, dynamic> json;
+
+  FakeEncryptedData(this.json);
+
+  @override
+  Map<String, dynamic> toJson() => json;
+}
+
+void main() {
+  setUpAll(() {
+    registerFallbackValue(TestDataModel(0));
+    registerFallbackValue(FakeEncryptedData(const <String, dynamic>{}));
+  });
+
+  group('SodiumRemoteAdapter', () {
+    final mockSodium = MockSodium();
+    final mockKeyManager = MockKeyManager();
+
+    late SutRemoteAdapter sut;
+
+    setUp(() {
+      reset(mockSodium);
+      reset(mockKeyManager);
+
+      sut = SutRemoteAdapter();
+
+      when(() => sut.sodium).thenReturn(mockSodium);
+      when(() => sut.keyManager).thenReturn(mockKeyManager);
+    });
+
+    test('creates DataCipher from sodium and keyManager', () {
+      expect(sut.cipher.keyManager, mockKeyManager);
+      expect(sut.cipher.sodium, mockSodium);
+    });
+
+    group('with cipher', () {
+      final mockDataCipher = MockDataCipher();
+
+      setUp(() {
+        reset(mockDataCipher);
+
+        sut = SutRemoteAdapter(mockDataCipher);
+        assert(sut.cipher == mockDataCipher);
+      });
+
+      test('serialize uses super.serialize and the encrypt data', () {
+        const testType = 'test-type';
+        const testSerializedData = {
+          'a': 1,
+          'b': true,
+          'c': 'yay',
+        };
+        final testEncryptedData = FakeEncryptedData(const <String, dynamic>{
+          'encrypted': true,
+        });
+
+        when(() => mockDataCipher.encrypt(any(), any()))
+            .thenReturn(testEncryptedData);
+        when(() => sut.type).thenReturn(testType);
+        when(() => sut.mockSerialize(any())).thenReturn(testSerializedData);
+
+        final testData = TestDataModel(42);
+        final result = sut.serialize(testData);
+
+        expect(result, testEncryptedData.json);
+        verifyInOrder([
+          () => sut.mockSerialize(testData),
+          () => mockDataCipher.encrypt(testType, testSerializedData),
+        ]);
+      });
+
+      test('deserialize forwards data to super.deserialize if not a map', () {
+        const testKey = 'test-key';
+        const testJson = [1, true, 'tree'];
+        final testData = DeserializedData([TestDataModel(42)]);
+
+        when(() => sut.mockDeserialize(any(), key: any(named: 'key')))
+            .thenReturn(testData);
+
+        final result = sut.deserialize(testJson, key: testKey);
+
+        expect(result, testData);
+
+        verify(() => sut.mockDeserialize(testJson, key: testKey));
+        verifyNever<dynamic>(() => mockDataCipher.decrypt(any(), any()));
+      });
+
+      test('deserialize first decrypt and the forwards to super.deserialize',
+          () {
+        const testType = 'test-type';
+        const testJson = {
+          'id': 42,
+          'cipherText': '',
+          'mac': '',
+          'nonce': '',
+          'hasAd': false,
+          'keyId': 111,
+        };
+        const testDecrypted = 42.42;
+        final testData = DeserializedData([TestDataModel(42)]);
+
+        when(() => sut.type).thenReturn(testType);
+        when(() => sut.mockDeserialize(any(), key: any(named: 'key')))
+            .thenReturn(testData);
+        when<dynamic>(() => mockDataCipher.decrypt(any(), any()))
+            .thenReturn(testDecrypted);
+
+        final result = sut.deserialize(testJson);
+
+        expect(result, testData);
+
+        verifyInOrder<dynamic>([
+          () => mockDataCipher.decrypt(
+                testType,
+                EncryptedData(
+                  id: 42,
+                  cipherText: Uint8List(0),
+                  mac: Uint8List(0),
+                  nonce: Uint8List(0),
+                  hasAd: false,
+                  keyId: 111,
+                ),
+              ),
+          () => sut.mockDeserialize(
+                testDecrypted,
+                key: any(named: 'key', that: isNull),
+              ),
+        ]);
+      });
+    });
+  });
+}
